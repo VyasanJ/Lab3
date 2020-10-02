4ace3431c1c0862d535101e688a18411d654cf05
VyasanJ <vyasan85@gmail.com>
2020-10-02 17:45:51 -0400

    Testing git commit

---
+++ src/BuddyInfo.java
@@ -0,0 +1,18 @@
+public class BuddyInfo {
+    private String name;
+
+    public BuddyInfo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Homer") ;
+        System.out.println("Hello " + buddy.getName());
+    }
+
+
+}
